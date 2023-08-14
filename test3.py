ab9fc6ff4d3054877dea4f4d3d975efbf074679f
Vedant Balapurkar <110763178+Vedantbalapurkar@users.noreply.github.com>
2023-08-14 15:24:58 +0530

    main.py

--- test3.py
+++ test3.py
@@ -1,89 +1,86 @@
-# from fastapi import FastAPI, UploadFile, File
-# from unstructured.partition.auto import partition
-# import uvicorn
-# import os
-# import json
-# import streamlit as st
-
-# import streamlit as st
-# import os
-# import json
-
-# # def partition(filename):
-# #     # Implement your partitioning logic here or call the appropriate function
-# #     # from the unstructured.partition.auto module
-# #     return ["element1", "element2", "element3"]
-
-# def main():
-#     st.title("File Upload and Partitioning")
-
-#     uploaded_file = st.file_uploader("Upload a file", type=["html", "txt", "pdf"])
-
-#     if uploaded_file is not None:
-#         # Read the content of the uploaded file
-#         content = uploaded_file.read()
-
-#         # Save the content to a temporary file
-#         temp_filename = "temp_file.html"  # Change the extension based on the actual file type
-#         with open(temp_filename, "wb") as temp_file:
-#             temp_file.write(content)
-
-#         # Call the partition function using the saved file path
-#         elements = partition(filename=temp_filename)
-
-#         # Clean up: remove the temporary file
-#         os.remove(temp_filename)
-
-#         elements_json = json.dumps(elements)
-        
-#         # Display the extracted elements
-#         st.write("Extracted elements:")
-#         for element in elements_json:
-#             st.write(element)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
 import streamlit as st
 import os
 import json
-from unstructured.partition.auto import partition  
+from unstructured.partition.auto import partition  # Make sure this import is correct
+import requests  # Import the requests library to fetch content from URL
+from unstructured.partition.html import partition_html
+
+def fetch_content_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        st.error("Failed to fetch content from the provided URL")
+        return None
 
 def main():
-    st.title("File Upload and Partitioning")
+    st.title("Content Partitioning")
 
-    uploaded_file = st.file_uploader("Upload a file", type=["html", "txt", "pdf"])
+    option = st.radio("Select an option:", ["Upload a File", "Enter a URL"])
 
-    if uploaded_file is not None:
-        # Read the content of the uploaded file
-        content = uploaded_file.read()
+    if option == "Upload a File":
+        uploaded_file = st.file_uploader("Upload a file", type=["html", "txt", "pdf"])
+        if uploaded_file is not None:
+            content = uploaded_file.read()
+            upload_content(content)
 
+    elif option == "Enter a URL":
+        url = st.text_input("Enter a URL to fetch content from")
+        if url:
+            content = fetch_content_from_url(url)
+            if content is not None:
+                url_content(content, url)
+
+def url_content(content, url):
+    # Call the partition function using the content
+    elements = partition_html(url=url, ssl_verify=False)
+
+    # Display the extracted elements
+    st.write("Extracted elements:")
+    for idx, element in enumerate(elements):
+        # Convert the element to a serializable format
+        serializable_element = {
+            "extracted_data": element.text  # Modify this to access the actual data you want to display/download
+        }
+        element_json = json.dumps(serializable_element, default=str, indent=2)
+        st.json(element_json)
+
+        # Add a download button for extracted content
+    extracted_content = element.text
+    extracted_filename = f"extracted_content_{idx}.txt"
+    st.download_button("Download Extracted Content", data=extracted_content, file_name=extracted_filename)
+
+
+
+
+
+def upload_content(content):
         # Save the content to a temporary file
-        temp_filename = "temp_file.html"  # Change the extension based on the actual file type
-        with open(temp_filename, "wb") as temp_file:
-            temp_file.write(content)
+    temp_filename = "temp_file.html"  # Change the extension based on the actual file type
+    with open(temp_filename, "wb") as temp_file:
+        temp_file.write(content)
 
         # Call the partition function using the saved file path
-        elements = partition(filename=temp_filename)  # Make sure the partition function is defined or imported
+    elements = partition(filename=temp_filename)  # Make sure the partition function is defined or imported
 
         # Clean up: remove the temporary file
-        os.remove(temp_filename)
+    os.remove(temp_filename)
 
-        # Display the extracted elements
-        # st.write("Extracted elements:")
-        # for element in elements:
-        #     element_json = json.dumps(element)  # Convert the element to JSON format
-        #     st.json(element_json)  # Display the JSON content
+        
 
 
-        for element in elements:
-            # Convert the element to a serializable format (e.g., extract relevant attributes)
-            #serializable_element = {"elements": element.elements}
-            element_json = json.dumps([str(element) for element in elements[::]])  # Convert the serializable element to JSON format
-            st.json(element_json)  # Display the JSON content
+    for content in elements:
+            
+        element_json = json.dumps([str(content) for content in elements[::]])  # Convert the serializable element to JSON format
+        st.json(element_json)
+
+    # Add a download button
+    extracted_content = "\n\n".join([json.dumps(content.__dict__, default=str, indent=2) for content in elements])
+    extracted_filename = "extracted_content.json"
+    st.download_button("Download Extracted Content", data=extracted_content, file_name=extracted_filename)
 
 if __name__ == "__main__":
     main()
+
+
+
