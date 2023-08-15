a18ac3f16107ce749006d62f4be2d2e6a798db94
Vedant Balapurkar <110763178+Vedantbalapurkar@users.noreply.github.com>
2023-08-15 23:34:45 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -4,6 +4,8 @@ import json
 from unstructured.partition.auto import partition  # Make sure this import is correct
 import requests  # Import the requests library to fetch content from URL
 from unstructured.partition.html import partition_html
+import urllib.parse
+
 
 def fetch_content_from_url(url):
     response = requests.get(url)
@@ -49,7 +51,9 @@ def url_content(url):
                 # Add a download button
     extracted_content = "\n\n".join([json.dumps(element.__dict__, default=str, indent=2) for element in elements])
     extracted_filename = "extracted_content.json"
-    st.download_button("Download Extracted Content", data=extracted_content, file_name=extracted_filename)
+    #st.download_button("Download Extracted Content", data=extracted_content, file_name=extracted_filename)
+    download_link = f'<a href="data:application/json;charset=utf-8,{urllib.parse.quote(extracted_content)}" download="{extracted_filename}">Download Extracted Content</a>'
+    st.markdown(download_link, unsafe_allow_html=True)
 
 
 
@@ -76,7 +80,9 @@ def upload_content(content):
     # Add a download button
     extracted_content = "\n\n".join([json.dumps(content.__dict__, default=str, indent=2) for content in elements])
     extracted_filename = "extracted_content.json"
-    st.download_button("Download Extracted Content", data=extracted_content, file_name=extracted_filename)
+    #st.download_button("Download Extracted Content", data=extracted_content, file_name=extracted_filename)
+    download_link = f'<a href="data:application/json;charset=utf-8,{urllib.parse.quote(extracted_content)}" download="{extracted_filename}">Download Extracted Content</a>'
+    st.markdown(download_link, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
